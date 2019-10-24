cc8b1e78ff1724db40179ecc65441ca617facdd3
sebastianhatnean <sebastian.hatnean@assist.ro>
2019-10-24 10:55:58 +0300

    Creating initial project structure

---
+++ src/app/components/shared/source-list/source-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-source-list',
+  templateUrl: './source-list.component.html',
+  styleUrls: ['./source-list.component.css']
+})
+export class SourceListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
