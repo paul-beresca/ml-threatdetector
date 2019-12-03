9e3e75ba715a8cb6efa733eeb4db3b3cfce868fd
Paul Beresca <paul.beresca@assist.ro>
2019-12-03 19:34:39 +0200

    SMS Api test

--- src/app/components/videos/video-settings/video-settings.component.ts
+++ src/app/components/videos/video-settings/video-settings.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit, Input } from "@angular/core";
-
+import { Component, OnInit, Input } from '@angular/core';
+import { VideosService } from 'src/app/services/videos.service';
 @Component({
-  selector: "app-video-settings",
-  templateUrl: "./video-settings.component.html",
-  styleUrls: ["./video-settings.component.scss"]
+  selector: 'app-video-settings',
+  templateUrl: './video-settings.component.html',
+  styleUrls: ['./video-settings.component.scss']
 })
 export class VideoSettingsComponent implements OnInit {
   @Input() videoSettings;
   location: Location;
 
-  constructor() {}
+  constructor(private videoService: VideosService) {}
 
   ngOnInit() {
     // this.location = {
     //   longitude: this.videoSettings.long,
     //   latitude: this.videoSettings.lat,
     // };
+  }
+
+  sendMessage() {
+    console.log('Entered message function');
+    const numbers = ['40742188066', '40751778431', '40740421159'];
+    const objToSend = {
+      user: 'paul.beresca',
+      pass: 'pk5C39bGS6Z6dC4',
+      dela: 'Engage',
+      mesaj: 'Acesta este un mesaj de test api de la Paul!'
+    };
+    const mesaj = objToSend.mesaj.replace(/\s+/g, '+');
+    numbers.map(number => {
+      this.videoService.messageCall(objToSend, mesaj, number);
+    });
+        // tslint:disable-next-line:max-line-length
+    // console.log(`https://www.whosms.ro/send.php?user=${objToSend.user}&pass=${objToSend.pass}&catre=${objToSend.catre}&dela=${objToSend.dela}&mesaj=${mesaj}`);
+    console.log('Message Sent!');
   }
 }
 
--- src/app/services/videos.service.ts
+++ src/app/services/videos.service.ts
@@ -8,6 +8,8 @@ export class VideosService {
 
   videosUrl: string = "https://jsonplaceholder.typicode.com/users";
   videosJson = "../../assets/VideoSources/data-videos.json";
+  // https://www.whosms.ro/send.php?user=paul.beresca&pass=pk5C39bGS6Z6dC4&catre=40742188066&dela=Engage&mesaj=MESAJ
+  smsUrl = `https://www.whosms.ro/send.php?user=paul.beresca&pass=pk5C39bGS6Z6dC4&catre=40742188066&dela=Engage&mesaj=MESAJ`;
 
   constructor(private http: HttpClient) { }
 
@@ -17,6 +19,13 @@ export class VideosService {
   
   getVideosFromJson() {
     return this.http.get(this.videosJson);
+  }
+
+  messageCall(objToSend, mesaj, number) {
+    // tslint:disable-next-line:max-line-length
+    return this.http.get(`https://www.whosms.ro/send.php?user=${objToSend.user}&pass=${objToSend.pass}&catre=${number}&dela=${objToSend.dela}&mesaj=${mesaj}`).subscribe(data => {
+      console.log('data', data);
+    });
   }
 }
   
