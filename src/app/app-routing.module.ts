3f6c753915f1d99228da96b7d5b1c131b9004644
Paul Beresca <paul.beresca@assist.ro>
2019-12-04 11:15:45 +0200

    Fixed whitespace on landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { ImageTrainToolComponent } from "./components/image-train-tool/image-train-tool.component";
-import { HomeComponent } from "./components/home/home.component";
-import { TensorflowExampleComponent } from "./components/tensorflow-example/tensorflow-example.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ImageTrainToolComponent } from './components/image-train-tool/image-train-tool.component';
+import { HomeComponent } from './components/home/home.component';
+import { TensorflowExampleComponent } from './components/tensorflow-example/tensorflow-example.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 const routes: Routes = [
-  { path: "home", component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   {
-    path: "",
-    redirectTo: "/landing",
-    pathMatch: "full"
+    path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full'
   },
-  { path: "imagetool", component: ImageTrainToolComponent },
-  { path: "tensorFlowExample", component: TensorflowExampleComponent },
-  { path: "landing", component: LandingPageComponent},
+  { path: 'imagetool', component: ImageTrainToolComponent },
+  { path: 'tensorFlowExample', component: TensorflowExampleComponent },
+  { path: 'landing', component: LandingPageComponent},
 ];
 
 @NgModule({
